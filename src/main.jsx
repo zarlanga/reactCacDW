4245d777caac4329431810e1c3b50a563bb52165
Replit user <>
2023-09-04 01:19:29 +0000

    traidoeljson

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Contador from './Roberto/contador.jsx'
+import ContadorRob from './Roberto/contador.jsx'
+import GrupoRob from './Roberto/grupo.jsx'
+
 
 const Pepe = (props) => <h1>{props.lala} buajajaja {props.ahre}</h1>;
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
  <>
-   <Contador/>
-   <Pepe lala="laconchadetumadrealboys" ahre="ahre"></Pepe>
-   
-   
- </>
+   <ContadorRob/>
+   <GrupoRob/>
+  </>
 )
 /*ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
